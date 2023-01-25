6ef6e6b19201da7b214896ea769071d96245848e
mahmoudhussein37 <hussienmahmoud34@gmail.com>
2023-01-26 00:41:54 +0200

    Now I finished the properties of the login page

--- EmpolyeeMgmt1/Login.cs
+++ EmpolyeeMgmt1/Login.cs
@@ -21,5 +21,20 @@ namespace EmpolyeeMgmt1
         {
 
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
