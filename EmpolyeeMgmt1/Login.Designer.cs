19a56e0b8516d17246c52199f489f63e3d1c7229
mahmoudhussein37 <hussienmahmoud34@gmail.com>
2023-01-25 19:11:54 +0200

    I added some items like a text box for the user name and the password, and Also button for the login

--- EmpolyeeMgmt1/Login.Designer.cs
+++ EmpolyeeMgmt1/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmpolyeeMgmt1
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
--- EmpolyeeMgmt1/Login.cs
+++ EmpolyeeMgmt1/Login.cs
@@ -1,10 +1,25 @@
 namespace EmpolyeeMgmt1
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
